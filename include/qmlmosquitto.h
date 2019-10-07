f3fc7e29436a71fb80355bb6dbdedd60147be581
Malcolm Sequeira <mal.seq@gmail.com>
2019-10-07 12:18:29 +0400

    qmlmosquitto - functionality updates

--- include/qmlmosquitto.h
+++ include/qmlmosquitto.h
@@ -8,20 +8,58 @@ class QMLMosquitto : public QQuickItem, public mosqpp::mosquittopp
 {
     Q_OBJECT
     Q_DISABLE_COPY(QMLMosquitto)
-    Q_PROPERTY(QString brokerAddress READ brokerAddress WRITE setBrokerAddress)
-    Q_PROPERTY(QString topic READ topic WRITE setTopic)
+    Q_PROPERTY(QString brokerAddress READ brokerAddress WRITE setBrokerAddress NOTIFY brokerAddressChanged)
+    Q_PROPERTY(int port READ port WRITE setPort NOTIFY portChanged)
+    Q_PROPERTY(int keepAlive READ keepAlive WRITE setKeepAlive NOTIFY keepAliveChanged)
+    Q_PROPERTY(QString topic READ topic WRITE setTopic NOTIFY topicChanged)
+    Q_PROPERTY(bool isConnected READ isConnected NOTIFY isConnectedChanged)
 public:
     QMLMosquitto(QQuickItem *parent = nullptr);
     ~QMLMosquitto();
+
+    // QProperties
     QString brokerAddress();
     void setBrokerAddress(QString addr);
     QString topic();
     void setTopic(QString topic);
+    int port();
+    void setPort(int p);
+    int keepAlive();
+    void setKeepAlive(int k);
+    bool isConnected();
+
+    // Invokables
+    Q_INVOKABLE void connect();
+    Q_INVOKABLE void disconnect();
+    Q_INVOKABLE void subscribe();
+    Q_INVOKABLE void unSubscribe();
+
     void componentComplete();
+
+    // Mosquitto Internals
+    void on_connect(int rc);
+    void on_disconnect(int rc);
+    void on_error();
+    void on_log(int, const char *);
+    void on_subscribe(int mid, int qos_count, const int *granted_qos);
+    void on_unsubscribe(int mid);
+    void on_message(const struct mosquitto_message *);
+
+signals:
+    void brokerAddressChanged(QString brokerAddress);
+    void portChanged(int port);
+    void keepAliveChanged(int keepAlive);
+    void topicChanged(QString newTopic);
+    void isConnectedChanged(bool isConnected);
+    void messageReceived(QString topic, QString payload);
 private:
     bool _component_complete;
     QString _broker;
     QString _topic;
+    bool _is_connected;
+
+    int _port;
+    int _keepalive;
 };
 
 #endif // QMLMOSQUITTO_H
--- src/qmlmosquitto.cpp
+++ src/qmlmosquitto.cpp
@@ -1,17 +1,21 @@
 #include "qmlmosquitto.h"
 
 QMLMosquitto::QMLMosquitto(QQuickItem *parent):
-    QQuickItem(parent),_component_complete(false)
+    QQuickItem(parent), mosqpp::mosquittopp("test"), _component_complete(false),
+    _is_connected(false), _port(1883), _keepalive(60)
 {
     // By default, QQuickItem does not draw anything. If you subclass
     // QQuickItem to create a visual item, you will need to uncomment the
     // following line and re-implement updatePaintNode()
 
     // setFlag(ItemHasContents, true);
+    qDebug()<<"QMLMosquitto::QMLMosquitto";
+    mosqpp::lib_init();
 }
 
 QMLMosquitto::~QMLMosquitto()
 {
+    mosqpp::lib_cleanup();
 }
 
 QString QMLMosquitto::brokerAddress()
@@ -22,7 +26,7 @@ QString QMLMosquitto::brokerAddress()
 void QMLMosquitto::setBrokerAddress(QString addr)
 {
     if(_component_complete){
-        qDebug()<<"QMLMosquitto::setBrokerAddress::Cannot set the broker address dynamically";
+        qDebug()<<"QMLMosquitto::setBrokerAddress::Cannot set the broker address";
     } else {
         qDebug()<<"QMLMosquitto::setBrokerAddress::addr="<<addr;
         _broker = addr;
@@ -37,15 +41,120 @@ QString QMLMosquitto::topic()
 void QMLMosquitto::setTopic(QString topic)
 {
     if(_component_complete){
-        qDebug()<<"QMLMosquitto::setTopic::Cannot set the topic dynamically";
+        qDebug()<<"QMLMosquitto::setTopic::Cannot set the topic";
     } else {
         qDebug()<<"QMLMosquitto::setTopic::topic="<<topic;
         _topic = topic;
     }
 }
 
+int QMLMosquitto::port()
+{
+    return _port;
+}
+
+void QMLMosquitto::setPort(int p)
+{
+    qDebug()<<"QMLMosquitto::setPort::port="<<p;
+    _port = p;
+}
+
+int QMLMosquitto::keepAlive()
+{
+    return _keepalive;
+}
+
+void QMLMosquitto::setKeepAlive(int k)
+{
+    qDebug()<<"QMLMosquitto::setKeepAlive::keepAlive="<<k;
+    _keepalive = k;
+}
+
 void QMLMosquitto::componentComplete()
 {
     qDebug()<<"QMLMosquitto::componentComplete";
-    _component_complete = true;
+    _component_complete = false;
+}
+
+void QMLMosquitto::on_connect(int rc)
+{
+    qDebug()<<"QMLMosquitto::on_connect::rc="<<rc;
+    if(rc == 0){
+        _is_connected = true;
+    } else {
+        _is_connected = false;
+    }
+    emit isConnectedChanged(_is_connected);
+}
+
+void QMLMosquitto::on_disconnect(int rc)
+{
+    qDebug()<<"QMLMosquitto::on_disconnect::rc="<<rc;
+    if(rc == 0){
+        _is_connected = false;
+        mosqpp::mosquittopp::loop_stop();
+    } else {
+        _is_connected = true;
+    }
+    emit isConnectedChanged(_is_connected);
+}
+
+void QMLMosquitto::on_error()
+{
+    qDebug()<<"QMLMosquitto::on_error";
+}
+
+void QMLMosquitto::on_log(int i, const char * str)
+{
+    qDebug()<<"QMLMosquitto::on_log::"<<i<<"::"<<str;
+}
+
+void QMLMosquitto::on_subscribe(int mid, int qos_count, const int *granted_qos)
+{
+    qDebug()<<"QMLMosquitto::on_subscribe::mid="<<mid<<", qos_count="<<qos_count<<", granted_qos"<<granted_qos;
+}
+
+void QMLMosquitto::on_unsubscribe(int mid)
+{
+    qDebug()<<"QMLMosquitto::on_unsubscribe::mid="<<mid;
+}
+
+void QMLMosquitto::on_message(const mosquitto_message *message)
+{
+    const char *payload = static_cast<const char* const>(message->payload);
+    qDebug()<<"QMLMosquitto::on_message::mid="<<message->mid
+           <<", topic="<<message->topic<<", payload="<<payload
+          <<", qos="<<message->qos<<", retain="<<message->retain;
+    emit messageReceived(QString(message->topic), QString(payload));
+}
+
+bool QMLMosquitto::isConnected()
+{
+    return _is_connected;
+}
+
+void QMLMosquitto::connect()
+{
+    qDebug()<<"QMLMosquitto::connect::broker="<<_broker<<", port="<<_port;
+    int ret = mosqpp::mosquittopp::connect_async(_broker.toUtf8().data(), _port, _keepalive, "192.168.0.118");
+    qDebug()<<"QMLMosquitto::connect::ret="<<ret<<"::"<<mosqpp::strerror(ret);
+    mosqpp::mosquittopp::loop_start();
+}
+
+void QMLMosquitto::disconnect()
+{
+    qDebug()<<"QMLMosquitto::disconnect";
+    mosqpp::mosquittopp::disconnect();
+}
+
+void QMLMosquitto::subscribe()
+{
+    int ret = mosqpp::mosquittopp::subscribe(NULL, _topic.toLatin1().data());
+    qDebug()<<"QMLMosquitto::subscribe::ret="<<ret;
+}
+
+void QMLMosquitto::unSubscribe()
+{
+    int ret = mosqpp::mosquittopp::unsubscribe(NULL, _topic.toLatin1().data());
+    qDebug()<<"QMLMosquitto::unSubscribe::ret="<<ret;
 }
